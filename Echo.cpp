6481cacb33b6f59c12744a1e7944ca4fbfbdb75c
carterawebb <carterwebb@nevada.unr.edu>
2022-04-30 20:06:44 -0700

    Echo Working

--- Echo.cpp
+++ Echo.cpp
@@ -3,7 +3,7 @@
 std::vector<float> Echo::process(const std::vector<float>& input, uint16_t numChan, uint16_t bps){
     std::vector<float> output;
     float gain = 0.5;
-    int delay = 1000;
+    int delay = 2000;
     auto decay = log(0.001)/log(gain);
     int newsize = int(input.size() + delay*decay);
     std::vector<float> longerInput(newsize, 0.0f);
@@ -21,18 +21,16 @@ std::vector<float> Echo::process(const std::vector<float>& input, uint16_t numCh
             }
         }
     } else if(numChan == 2){ //Stereo
-        for(int i = 0; i < newsize; i++){
-            if(i < delay){
-                output.push_back(0.0f);
-                output.push_back(0.0f);
-            }
-            else if(output[i-delay] > 0.0f){
-                output.push_back(input[i] + gain * input[i - delay]);
-                output.push_back(input[i + 1] + gain * input[i - delay + 1]);
-            } else {
+        for(int i = 0; i < newsize; i+=2){
+            if(i < delay*decay){
                 output.push_back(input[i]);
-                output.push_back(input[i + 1]);
-
+                output.push_back(input[i+1]);
+            }
+            else{
+                output.push_back(longerInput[i]*gain + input[i]);
+                output.push_back(longerInput[i+1]*gain + input[i+1]);
+                longerInput[i] += output[i]*decay;
+                longerInput[i+1] += output[i+1]*decay;
             }
         }
     }
