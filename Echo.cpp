ba46f7b9e71c36afdd8efacbc0ae1fc514903624
Vincent Lee <vlee2@nevada.unr.edu>
2022-04-30 20:59:37 -0700

    echo test

--- Echo.cpp
+++ Echo.cpp
@@ -1,9 +1,10 @@
 #include "Echo.h"
+#include <iostream>
 
 std::vector<float> Echo::process(const std::vector<float> input, wav_hdr attributes){
     std::vector<float> output;
     float gain = 0.5;
-    int delay = 5000;
+    int delay = 3500;
     auto decay = log(0.001)/log(gain);
     int newsize = int(input.size() + delay*decay);
     std::vector<float> longerInput(newsize, 0.0f);
@@ -21,6 +22,7 @@ std::vector<float> Echo::process(const std::vector<float> input, wav_hdr attribu
             }
         }
     } else if(attributes.NumChannels == 2){ //Stereo
+        std::cout << '1' << std::endl;
         for(int i = 0; i < newsize; i+=2){
             if(i < delay*decay){
                 output.push_back(input[i]);
@@ -33,6 +35,7 @@ std::vector<float> Echo::process(const std::vector<float> input, wav_hdr attribu
                 longerInput[i+1] += output[i+1]*decay;
             }
         }
+        std::cout << '2' << std::endl;
     }
     return output;
 }
