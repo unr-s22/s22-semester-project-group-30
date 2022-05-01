f06d75ea1383372d8be3497a59d79d98b4ab5006
Vincent Z Bongarzone <vbongarzone@nxlogin.rd.unr.edu>
2022-04-30 17:22:02 -0700

    else error

--- Controller.cpp
+++ Controller.cpp
@@ -28,9 +28,12 @@ void Controller::run() {
     if(processChoice == 1){ 
         vector<float> const &outputAudioEcho = echo.process(inputAudio, model.getAttribute().NumChannels, model.getAttribute().BitsPerSample);
         UI.setAudio(outputAudioEcho, model.getAttribute().Subchunk1Size, model.getAttribute().AudioFormat, model.getAttribute().NumChannels, model.getAttribute().SampleRate, model.getAttribute().BitsPerSample, model.getAttribute().Subchunk2Size);
-    } else if(processChoice == 2){
+    }
+	else if(processChoice == 2){
         vector<float> const &outputAudioNormalizer = normalizer.process(inputAudio, model.getAttribute().NumChannels, model.getAttribute().BitsPerSample);
         UI.setAudio(outputAudioNormalizer, model.getAttribute().Subchunk1Size, model.getAttribute().AudioFormat, model.getAttribute().NumChannels, model.getAttribute().SampleRate, model.getAttribute().BitsPerSample, model.getAttribute().Subchunk2Size);
     }
-
+	else {
+		cout << "Error: Invalid choice." << endl;
+	}
 }
